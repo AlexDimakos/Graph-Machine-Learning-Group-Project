80560e451a7cbcc8e69a4dd2f47db63eed140970
Konstantinos Rizavas <rizavask@gmail.com>
2025-10-16 18:52:16 +0200

    Completed training pipeline for lstm

--- src/config.py
+++ src/config.py
@@ -1,4 +1,7 @@
+from dataclasses import dataclass
 from pathlib import Path
+
+import torch
 
 # Paths
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
@@ -8,3 +11,18 @@ RAW_DATA_DIR = DATA_DIR / "raw"
 PROCESSED_DATA_DIR = DATA_DIR / "processed"
 MODELS_DIR = PROJECT_ROOT / "outputs/models"
 MLFLOW_DIR = PROJECT_ROOT / "outputs/mlflow"
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+@dataclass
+class LSTMConfig:
+    n_splits = 5
+    window_size = 10
+    batch_size = 64
+    hidden_size = 16
+    epochs = 100
+    lr = 0.001
+    eval_every = 5
+    patience = 10
+    save_path = Path("outputs/models/lstm.pt")
--- src/models/train.py
+++ src/models/train.py
@@ -1,13 +1,25 @@
+import sys
+from pathlib import Path
+
 import numpy as np
+import pandas as pd
 import torch
 import torch.nn as nn
+from sklearn.discriminant_analysis import StandardScaler
+from sklearn.model_selection import TimeSeriesSplit
 from torch.utils.data import DataLoader
+
+# add parent directory of src to Python path
+sys.path.append(str(Path(__file__).resolve().parents[2]))
+
+from src.config import *
+from src.data.dataset import LSTMDataset
+from src.models.model import LSTMBaseline
 
 
 def evaluate_model(model, dataset, batch_size=64):
     model.eval()
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model.to(device)
+    model.to(DEVICE)
     criterion = nn.MSELoss()
 
     loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
@@ -16,8 +28,8 @@ def evaluate_model(model, dataset, batch_size=64):
 
     with torch.no_grad():
         for batch in loader:
-            x = batch["x"].to(device)
-            y_true = batch["y"].to(device)
+            x = batch["x"].to(DEVICE)
+            y_true = batch["y"].to(DEVICE)
             y_pred = model(x)
 
             loss = criterion(y_pred, y_true)
@@ -44,8 +56,7 @@ def train_lstm(
     patience=10,
     save_path="best_model.pt",
 ):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model.to(device)
+    model.to(DEVICE)
     criterion = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
@@ -68,8 +79,8 @@ def train_lstm(
         total_loss = 0.0
 
         for batch in train_loader:
-            x = batch["x"].to(device)
-            y = batch["y"].to(device)
+            x = batch["x"].to(DEVICE)
+            y = batch["y"].to(DEVICE)
 
             optimizer.zero_grad()
             y_pred = model(x)
@@ -85,7 +96,7 @@ def train_lstm(
         # Periodic evaluation
         if val_dataset is not None and epoch % eval_every == 0:
             val_loss, val_rmse, _, _ = evaluate_model(
-                model, val_dataset, batch_size, criterion, device
+                model, val_dataset, LSTMConfig.batch_size
             )
             history["val_loss"].append(val_loss)
             history["val_rmse"].append(val_rmse)
@@ -116,92 +127,119 @@ def train_lstm(
     return model, history
 
 
-# def train_and_evaluate():
-#     tscv = TimeSeriesSplit(n_splits=5)
-#     """
-#     {
-#         "k-fold": {
-#             model,
-#             best_val_loss,
-#             best_val_rmse
-#         }
-#     }
-#     """
-#     cv_results = {}
+def scale_per_product(X_train, y_train, X_val, y_val):
+    # Prepare scaled containers
+    X_train_scaled = np.zeros_like(X_train)
+    y_train_scaled = np.zeros_like(y_train)
+    X_val_scaled = np.zeros_like(X_val)
+    y_val_scaled = np.zeros_like(y_val)
 
-#     for i, (train_idx, test_idx) in enumerate(tscv.split(X)):
-#         X_train = X[train_idx]
-#         X_test = X[test_idx]
-#         y_train = y[train_idx]
-#         y_test = y[test_idx]
+    scaler_X_per_product = []
+    scaler_y_per_product = []
 
-#         # Prepare scaled containers
-#         X_train_scaled = np.zeros_like(X_train)
-#         y_train_scaled = np.zeros_like(y_train)
-#         X_test_scaled = np.zeros_like(X_test)
-#         y_test_scaled = np.zeros_like(y_test)
+    T, N, F = X_train.shape
+    for node_idx in range(N):
+        # Fit a scaler for features of this product
+        scaler_X = StandardScaler()
+        scaler_y = StandardScaler()
 
-#         scaler_X_per_product = []
-#         scaler_y_per_product = []
+        X_node = X_train[:, node_idx, :]  # (T, F)
+        y_node = y_train[:, node_idx].reshape(-1, 1)
 
-#         T, N, F = X_train.shape
-#         for node_idx in range(N):
-#             # Fit a scaler for features of this product
-#             scaler_X = StandardScaler()
-#             scaler_y = StandardScaler()
+        X_train_scaled[:, node_idx, :] = scaler_X.fit_transform(X_node)
+        y_train_scaled[:, node_idx] = scaler_y.fit_transform(y_node).ravel()
 
-#             X_node = X_train[:, node_idx, :]  # (T, F)
-#             y_node = y_train[:, node_idx].reshape(-1, 1)
+        # Scale val data using same scaler
+        X_val_scaled[:, node_idx, :] = scaler_X.transform(X_val[:, node_idx, :])
+        y_val_scaled[:, node_idx] = scaler_y.transform(
+            y_val[:, node_idx].reshape(-1, 1)
+        ).ravel()
 
-#             X_train_scaled[:, node_idx, :] = scaler_X.fit_transform(X_node)
-#             y_train_scaled[:, node_idx] = scaler_y.fit_transform(y_node).ravel()
+        scaler_X_per_product.append(scaler_X)
+        scaler_y_per_product.append(scaler_y)
 
-#             # Scale test data using same scaler
-#             X_test_scaled[:, node_idx, :] = scaler_X.transform(X_test[:, node_idx, :])
-#             y_test_scaled[:, node_idx] = scaler_y.transform(
-#                 y_test[:, node_idx].reshape(-1, 1)
-#             ).ravel()
+    return (
+        X_train_scaled,
+        y_train_scaled,
+        X_val_scaled,
+        y_val_scaled,
+        scaler_X_per_product,
+        scaler_y_per_product,
+    )
 
-#             scaler_X_per_product.append(scaler_X)
-#             scaler_y_per_product.append(scaler_y)
 
-#         window_size = int(
-#             0.1 * T
-#         )  # TODO: Consider here the implications of window size and the possibility of training a rolling LSTM with no fixed window size
-#         batch_size = 64
+def run_lstm_training_pipeline(X_train, y_train, X_val, y_val):
+    X_train, y_train, X_val, y_val, scaler_X_per_product, scaler_y_per_product = (
+        scale_per_product(X_train, y_train, X_val, y_val)
+    )
 
-#         train_dataset = GraphTimeSeriesDataset(
-#             X_train_scaled, y_train_scaled, window_size=window_size
-#         )
-#         test_dataset = GraphTimeSeriesDataset(
-#             X_test_scaled, y_test_scaled, window_size=window_size
-#         )
+    train_dataset = LSTMDataset(X_train, y_train, window_size=LSTMConfig.window_size)
+    val_dataset = LSTMDataset(X_val, y_val, window_size=LSTMConfig.window_size)
 
-#         train_dataloader = DataLoader(
-#             train_dataset, batch_size=batch_size, shuffle=True
-#         )
-#         test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    input_size = X_train.shape[2]  # number of features (F)
+    model = LSTMBaseline(input_size, LSTMConfig.hidden_size).to(DEVICE)
 
-#         input_size = F  # number of features per node
-#         hidden_size = 16
+    model, history = train_lstm(
+        model,
+        train_dataset,
+        val_dataset=val_dataset,
+        batch_size=LSTMConfig.batch_size,
+        num_epochs=LSTMConfig.epochs,
+        lr=LSTMConfig.lr,
+        eval_every=LSTMConfig.eval_every,
+        patience=LSTMConfig.patience,
+        save_path=LSTMConfig.save_path,
+    )
 
-#         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    return model, history
 
-#         model = LSTMBaseline(input_size, hidden_size).to(device)
 
-#         model, history = train_lstm(
-#             model,
-#             train_dataset,
-#             val_dataset=test_dataset,
-#             window_size=5,
-#             batch_size=batch_size,
-#             num_epochs=100,
-#             lr=0.001,
-#             eval_every=1,
-#             patience=20,
-#         )
-#         cv_results[f"fold_{i}"] = {
-#             "model": model,
-#             "best_val_loss": history["best_val_loss"],
-#             "best_val_rmse": history["best_val_rmse"],
-#         }
+def cross_validation_training(X, y):
+    tscv = TimeSeriesSplit(n_splits=LSTMConfig.n_splits)
+    """
+    {
+        "fold_k": {
+            model,
+            best_val_loss,
+            best_val_rmse
+        }
+    }
+    """
+    cv_results = {}
+
+    for i, (train_idx, val_idx) in enumerate(tscv.split(X)):
+        X_train = X[train_idx]
+        y_train = y[train_idx]
+        X_val = X[val_idx]
+        y_val = y[val_idx]
+
+        model, history = run_lstm_training_pipeline(X_train, y_train, X_val, y_val)
+
+        cv_results[f"fold_{i}"] = {
+            "model": model,
+            "best_val_loss": history["best_val_loss"],
+            "best_val_rmse": history["best_val_rmse"],
+        }
+
+    # TODO: add better processing of results
+    # Convert cv_results to a DataFrame
+    results_df = pd.DataFrame(cv_results)
+
+    # Calculate mean and std of best_val_loss
+    mean_loss = results_df.loc["best_val_rmse"].mean()
+    std_loss = results_df.loc["best_val_rmse"].std()
+
+    print(f"Mean best validation rmse: {mean_loss:.4f} ± {std_loss:.4f}")
+
+
+def main():
+    X, y = np.load(PROCESSED_DATA_DIR / "X.npy"), np.load(PROCESSED_DATA_DIR / "y.npy")
+    cross_validation_training(X, y)
+    # print("mpla")
+    # for el in str(Path(__file__).resolve().parents):
+    #     print(el)
+    # # print(str(Path(__file__).resolve().parents))
+
+
+if __name__ == "__main__":
+    main()
